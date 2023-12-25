bab067f8c046c387bbd9d14c74c8776035eb78c9
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-12-25 22:20:29 +0200

    Inject analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import App from "./App.tsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
+import { inject } from "@vercel/analytics";
 import { store, persistor } from "./store/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
 import { CircularProgress } from "@mui/material";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
